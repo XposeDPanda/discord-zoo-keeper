7c2b6d381c0c1b8a289e6242357eedeb4af4201a
Callum Clarke <xposedpanda@gmail.com>
2020-08-25 01:37:54 +0100

    Basic bot logs in and listens for messages

---
+++ bot.py
@@ -0,0 +1,18 @@
+TOKEN = "ENTER TOKEN HERE"
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
