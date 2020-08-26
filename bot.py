924f009de8f5edebb619bce7609d1538cedca4cc
Callum Clarke <xposedpanda@gmail.com>
2020-08-26 20:27:20 +0100

    bot responds to image and now gives role

--- bot.py
+++ bot.py
@@ -19,8 +19,17 @@ async def on_message(message):
     if len(message.attachments) > 0:
         ## Attachment found
         imageURL = message.attachments[0].url
-        await utils.title.get_title(imageURL)
-
+        title = await utils.title.get_title(imageURL)
+        if title == 'Pirate Legend':
+            try:
+                role = discord.utils.get(message.guild.roles, name=title)
+                await message.author.add_roles(role)
+                await message.add_reaction('\U0001f44d')
+                return
+            except ValueError:
+                await message.channel.send('I can see your title is: {0} however I cannot seem to find the correct role to give you, please contact server management.'.format(title))
+        else:
+            await message.channel.send('Unfortunetly I could not find the Pirate Legend title in your screenshot, please equip the title to your character and try again. \n Title found: {0}'.format(title))
     else:
         ## No attachment found
         return
